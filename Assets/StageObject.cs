30a9c20a526a1d2a90c259d2321532e766da0ab4
MoHoDu <imvagueness@gmail.com>
2023-01-10 19:22:44 +0900

    Fixed StageObject script
    - fixed stageObject script

--- Assets/StageObject.cs
+++ Assets/StageObject.cs
@@ -1,12 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+//public class Word : ScriptableObject
+//{
+//    public string name;
+//    public NewAdjective[] newAdjectives;
+//}
+
+//[CreateAssetMenu(menuName = "create Name", order = 0)]
+//public class Name : Word
+//{
+//    public Nametype nametype;  // 생성할 때만 쓸것, 카드로 줄 때는 안 씁니다.
+//}
+
+//[CreateAssetMenu(menuName = "create Adjective", order = 1)]
+//public class Adjective : Word
+//{
+
+//}
 
 public struct NewAdjective
 {
     public int index;
     public bool value;
 }
+
+//public class TestButton : MonoBehaviour
+//{
+//    public Adjective[] adjectives;
+//    public Name name;
+//    public GameObject targetObj;
+
+//    public void SetObj()
+//    {
+//        StageObject sObj = targetObj.GetComponent<StageObject>();
+//        sObj.SetObject(name.name, name.newAdjectives);
+//        foreach (Adjective adj in adjectives)
+//            sObj.SetObject(adj.name + " " + sObj.Name, adj.newAdjectives);
+//    }
+//}
+
+//public enum Nametype
+//{
+//    none = -1,
+//    tile = 0,
+//    rock = 1
+//}
+
+//public class LevelEditor
+//{
+//    public void CreatePrefab(Nametype nt)
+//    {
+
+//        GameObject obj = Resources.Load(nt.ToString()) as GameObject;
+//        GameObject.Instantiate(obj);
+//    }
+//}
 
 public class StageObject : MonoBehaviour
 {
@@ -16,9 +67,14 @@ public class StageObject : MonoBehaviour
         get { return name; }
         set { if (value == "") return; name = value; }
     }
-    private bool[] specificity;
+    [SerializeField] private bool[] specificity;
 
     public StageObject(string name, NewAdjective[] adjectives)
+    {
+        SetObject(name, adjectives);
+    }
+
+    public void SetObject(string name, NewAdjective[] adjectives)
     {
         this.name = name;
         SetSpeficity(adjectives);
