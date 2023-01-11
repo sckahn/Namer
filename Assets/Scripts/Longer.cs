da1659812e4121c6ee5d4f6203362e589b19c96a
J1nTech <106944708+J1nTech@users.noreply.github.com>
2023-01-11 16:32:05 +0900

    Update Longer.cs

--- Assets/Scripts/Longer.cs
+++ Assets/Scripts/Longer.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class Longer : MonoBehaviour
 {
+    private int growScale = 2;
+    private float goalScale;
+    private float currentHeight;
     [ContextMenu("GrowLonger")]
     public void GrowLonger()
     {
-        Vector3 currentScale = transform.localScale;
-        float newHeight = currentScale.y * 2;
-        Vector3 newScale = new Vector3(currentScale.x, newHeight, currentScale.z);
-        transform.localScale = newScale;
+        SetGoalScale();
+        StartCoroutine(Grow());
+        
+    }
+
+    public void SetGoalScale()
+    {
+        goalScale = transform.localScale.y * growScale;
+        currentHeight = transform.localScale.y;
+    }
+
+    IEnumerator Grow()
+    {
+        while (currentHeight <= goalScale)
+        {
+            currentHeight += 0.1f;
+            Vector3 newScale = new Vector3(transform.localScale.x, currentHeight, transform.localScale.z);
+            transform.localScale = newScale;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
