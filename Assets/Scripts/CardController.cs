a7538e382b1a91134f90f3643f428a376af40557
heokiro <87606800+heokiro@users.noreply.github.com>
2023-01-12 17:00:03 +0900

    [Update] Tutorial Scene
    Create Ball, Long, Pop CardPrefabs,
    Create Tutorial Scene

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CardController : MonoBehaviour
 {
+    [SerializeField] GameObject backCover;
+    [SerializeField] GameObject frontCover;
+
     public PRS originPRS;
+
+    public void MoveTransform(PRS prs, bool useDotween, float dotweenTime = 0)
+    {
+        if (useDotween)
+        {
+            transform.DOMove(prs.pos, dotweenTime);
+            transform.DORotateQuaternion(prs.rot, dotweenTime);
+            transform.DOScale(prs.scale, dotweenTime);
+        }
+        else
+        {
+            transform.position = prs.pos;
+            transform.rotation = prs.rot;
+            transform.localScale = prs.scale;
+        }
+
+    }
+
+    private void Update()
+    {
+        //this.transform.LookAt(Camera.main.transform);
+    }
 }
--- Assets/Scripts/Manager/CardManager.cs
+++ Assets/Scripts/Manager/CardManager.cs
@@ -6,10 +6,71 @@ public class CardManager : Singleton<CardManager>
 {
     [SerializeField] GameObject cardPrefab;
     [SerializeField] Transform cardSpawnPoint;
+    [SerializeField] Transform cardHolderPoint;
+    [SerializeField] Transform cardHolderLeft;
+    [SerializeField] Transform cardHolderRight;
+    [SerializeField] List<CardController> myCards;
 
+    //카드를 생성하는 메서드 
+    [ContextMenu("AddCard")]
     void AddCard()
     {
         var cardObject = Instantiate(cardPrefab, cardSpawnPoint.position, Quaternion.identity);
+        var card = cardObject.GetComponent<CardController>();
+        myCards.Add(card);
+        CardAlignment();
+    }
+
+    //카드를 정렬하는 메서드 
+    void CardAlignment()
+    {
+        List<PRS> originCardPRSs = new List<PRS>();
+        originCardPRSs = RoundAlignment(cardHolderLeft, cardHolderRight, myCards.Count, 0.5f, new Vector3(0.4f, 0.6f, 0.01f));
+
+        for (int i = 0; i < myCards.Count; i++)
+        {
+            var targetCard = myCards[i];
+
+            targetCard.originPRS = originCardPRSs[i];
+            Debug.Log(cardHolderPoint.position);
+            targetCard.MoveTransform(targetCard.originPRS, true, 2f);
+        }
+    }
+
+    //카드를 둘글게 정렬하는 메서드
+    List<PRS> RoundAlignment(Transform leftTr, Transform rightTr, int objCount, float height, Vector3 scale)
+    {
+        float[] objLerps = new float[objCount];
+        List<PRS> results = new List<PRS>(objCount);
+
+        switch (objCount)
+        {
+            case 1: objLerps = new float[] { 0.5f }; break;
+            case 2: objLerps = new float[] { 0.24f, 0.73f }; break;
+            case 3: objLerps = new float[] { 0.1f, 0.5f, 0.9f }; break;
+            default:
+                float interval = 1f / (objCount - 1);
+                for (int i = 0; i < objCount; i++)
+                {
+                    objLerps[i] = interval * i;
+                }
+                break;
+        }
+
+        for (int i = 0; i < objCount; i++)
+        {
+            var targetPos = Vector3.Lerp(leftTr.position, rightTr.position, objLerps[i]);
+            var targetRot = Quaternion.identity;
+            if (objCount >= 4)
+            {
+                float curve = Mathf.Sqrt(Mathf.Pow(height, 2) - Mathf.Pow(objLerps[i] - 0.5f, 2));
+                curve = height >= 0 ? curve : -curve;
+                targetPos.y += curve;
+                targetRot = Quaternion.Slerp(leftTr.rotation, rightTr.rotation, objLerps[i]);
+            }
+            results.Add(new PRS(targetPos, targetRot, scale));
+        }
+        return results;
     }
 
 }
