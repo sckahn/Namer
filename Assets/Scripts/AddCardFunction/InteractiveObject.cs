493c78c64990fd8cc7432de66e3024df42be7c88
newHj9 <108648161+newHj9@users.noreply.github.com>
2023-01-18 16:09:58 +0900

    InteractiveObject에서 Material 변수 삭제

--- Assets/Scripts/AddCardFunction/InteractiveObject.cs
+++ Assets/Scripts/AddCardFunction/InteractiveObject.cs
@@ -13,7 +13,6 @@ public class InteractiveObject : MonoBehaviour
     [SerializeField] GameObject popUpName;
 
     private Vector3 currentPosition;
-    private Material currentMaterial;
     
     private string currentObjectName = "???";
     private string addNameText;
@@ -48,8 +47,7 @@ public class InteractiveObject : MonoBehaviour
     private void OnEnable()
     {
         currentPosition = gameObject.transform.position;
-        currentMaterial = transform.GetChild(0).GetComponent<MeshRenderer>().material;
-        
+
         nameData = FindObjectOfType<NameData>();
 
         if (!gameObject.CompareTag("InteractObj"))
