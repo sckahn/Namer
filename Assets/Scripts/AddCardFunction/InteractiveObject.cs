dcdc97e292c9d00389183a5204802852586bf323
heokiro <87606800+heokiro@users.noreply.github.com>
2023-01-18 17:48:02 +0900

    [Add] Pause UI
    NC-89
    1.플레이 중 ESC를 누르면 나오는 일시정지 UI 구현
    2. 일시 정지 중 게임화면 멈춤, 선택 불가 구현

--- Assets/Scripts/AddCardFunction/InteractiveObject.cs
+++ Assets/Scripts/AddCardFunction/InteractiveObject.cs
@@ -296,6 +296,7 @@ public class InteractiveObject : MonoBehaviour
     bool isHoverling = false;
     private void OnMouseOver()
     {
+        if (UIManager.GetInstance.isPause) return;
         isHoverling = true;
         if (this.gameObject.CompareTag("InteractObj") && CardManager.GetInstance.isPickCard)
         {
@@ -311,6 +312,7 @@ public class InteractiveObject : MonoBehaviour
     //오브젝트의 이름을 화면에서 가림 
     private void OnMouseExit()
     {
+        if (UIManager.GetInstance.isPause) return;
         isHoverling = false;
         CardManager.GetInstance.target = null;
         popUpName.SetActive(false);
