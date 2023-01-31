e42d2a97ca0c403d7f9b68a4afd0146834272491
rlaqudgus <108641304+rlaqudgus@users.noreply.github.com>
2023-01-31 14:44:04 +0900

    [Change] fire 애셋 추가 Flammable 효과 변경
    NC-92
    1) Hovl studio 애셋 임포트
    2) 애셋에서 화염 효과를 Flammable adj 스크립트에 추가 및 자연스러워보이게 코루틴 시간 조절

--- Assets/Scripts/AddCardFunction/Adjective/FlammableAdj.cs
+++ Assets/Scripts/AddCardFunction/Adjective/FlammableAdj.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
-public class FlammableAdj : MonoBehaviour, IAdjective
+public class FlammableAdj : IAdjective
 {
     private EAdjective adjectiveName = EAdjective.Flammable;
-    private EAdjectiveType adjectiveType = EAdjectiveType.Contradict;
+    private EAdjectiveType adjectiveType = EAdjectiveType.Normal;
     private int count = 0;
-    
+
+    #region 잘타는 꾸밈 카드 맴버변수
+    private GameObject fireEffect;
+    private ParticleSystem fire;
+    private bool isOnFire;
     private bool isContact;
-    [Range(0,1)]public float sweepDistance = .5f;
+    #endregion
 
     public EAdjective GetAdjectiveName()
     {
@@ -30,54 +38,139 @@ public class FlammableAdj : MonoBehaviour, IAdjective
     {
         this.count += addCount;
     }
-    
+
     public void Execute(InteractiveObject thisObject)
     {
-        ObjectOnFire();
+        ParticleSetting(thisObject);
+        ObjectOnFire(thisObject);
+        Debug.Log("execute1");
     }
 
     public void Execute(InteractiveObject thisObject, GameObject player)
     {
         //Debug.Log("Flammable : this Object -> Player");
+        // ObjectOnFire(thisObject);
     }
-    
+
     public void Execute(InteractiveObject thisObject, InteractiveObject otherObject)
     {
-        //Debug.Log("Flammable : this Object -> other Object");
+        Debug.Log("Flammable : this Object -> other Object");
+        if (otherObject.Adjectives[(int)EAdjective.Flame].GetAdjectiveName() == EAdjective.Flame)
+        {
+            isContact = true;
+            ObjectOnFire(thisObject.gameObject);
+        }
     }
-    
+
     [ContextMenu("Flammable Testing")]
-    public void ObjectOnFire()
+    private void ObjectOnFire(InteractiveObject targetObj)
     {
-        LookUpFlame();
-        if (isContact )
+        LookUpFlame(targetObj.gameObject);
+        if (isContact)
         {
             isContact = false;
-            gameObject.SetActive(false);
+            isOnFire = true;
+            targetObj.StartCoroutine(OnFire(targetObj.gameObject));
+            isOnFire = false;
+            // targetObj.gameObject.SetActive(false);
             // print("Boom");
         }
     }
 
-    
-    private void LookUpFlame()
+    private void ObjectOnFire(GameObject targetObj)
     {
-        var neighbors = GameManager.GetInstance.GetCheckSurrounding.CheckNeighboursObjectsUsingSweepTest(gameObject,sweepDistance);
-
-        for (int i = 0; i < neighbors.Count; i++)
+        // LookUpFlame(targetObj.gameObject); // 이거 이중 체크 인데..
+        if (isContact)
         {
-            if ((Dir)i != Dir.up && (Dir)i != Dir.down)
+            isContact = false;
+            isOnFire = true;
+            targetObj.GetComponent<InteractiveObject>().StartCoroutine(OnFire(targetObj.gameObject));
+            isOnFire = false;
+            // targetObj.gameObject.SetActive(false);
+            // print("Boom");
+        }
+    }
+
+
+
+    private void LookUpFlame(GameObject targetObj)
+    {
+
+        Dictionary<Dir, List<Transform>> crossDirDict = new Dictionary<Dir, List<Transform>>();
+        for (int i = 0; i < Enum.GetNames(typeof(Dir)).Length; i++)
+        {
+            if ((Dir)i != Dir.down && (Dir)i != Dir.up && (Dir)i != Dir.Null)
             {
-                foreach (var item in neighbors[(Dir)i])
+                var objList = GameManager.GetInstance.GetCheckSurrounding.GetTransformsAtDirOrNull(targetObj, (Dir)i);
+                if (objList != null)
+                    crossDirDict.Add((Dir)i, objList);
+            }
+        }
+        foreach (var ol in crossDirDict)
+        {
+            foreach (var item in ol.Value)
+            {
+                Debug.Log(item.name + ol.Key);
+                Debug.Log(item.gameObject.name);
+                //interactiveobject component가 없을 경우 에러뜸 임시로 예외처리 해줌
+                if (item.gameObject.GetComponent<InteractiveObject>() == null) continue;
+
+                if (item.gameObject.GetComponent<InteractiveObject>().Adjectives[(int)EAdjective.Flame]
+                        .GetAdjectiveName() == EAdjective.Flame)
                 {
-                    // item object의 오브젝트 클레스 안에 불값으로 특성이 있는지 없는지 가져오기
-                    //bool flag =CheckAdj(item) 이거 구현 해달라고 요청
-                    if (item.gameObject.name == "Flame")
-                    {
-                        isContact = true;
-                        return;
-                    }
+                    isContact = true;
                 }
             }
         }
+        Debug.Log(isContact);
     }
+
+    GameObject FindEffect(String prefabName)
+    {
+        string path = "Assets/Prefabs/Effect/" + prefabName + ".prefab";
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        if (prefab == null)
+        {
+            Debug.LogError("Prefab not found: " + prefabName);
+        }
+
+        return prefab;
+    }
+
+    IEnumerator OnFire(GameObject thisObj)
+    {
+        fire.Play();
+
+        yield return new WaitForSeconds(2.5f);
+
+        var ObjectMesh = thisObj.GetComponentInChildren<MeshRenderer>();
+        ObjectMesh.enabled = false;
+        fire.Stop();
+
+        Vector3 pos = thisObj.transform.position;
+
+        Debug.Log("Pos : " + pos.x + pos.y + pos.z);
+
+        DetectManager.GetInstance.ChangeValueInMap(pos,null);
+
+        yield return new WaitForSeconds(1.5f);
+        GameObject.Destroy(thisObj);
+        //while (isOnFire)
+        //{
+        //    fire.Play();
+        //    yield return new WaitForSeconds(5f);
+        //    //waith until phase has changed
+        //    // yield return new WaitUntil();
+        //    // yield return new DOTweenCYInstruction.WaitForCompletion();
+        //}
+    }
+
+    private void ParticleSetting(InteractiveObject thisObject)
+    {
+        fireEffect = FindEffect("Fire effect2");
+        GameObject.Instantiate(fireEffect, thisObject.transform);
+        fire = thisObject.gameObject.GetComponentInChildren<ParticleSystem>();
+        fire.Stop();
+    }
+
 }
