5c179be939120e45ca281c7bab9b05a7790be137
J1nTech <106944708+J1nTech@users.noreply.github.com>
2023-02-01 16:00:45 +0900

    [Fix] LightAdj 함수 변경
    NC-68
    1) 게임 오브젝트를 생성 -> 프리펩을 이용한 instantiate으로 변경
    2) Abandon 내용구현 -> 생성된 프리펩을 destroy

--- Assets/Scripts/AddCardFunction/Adjective/LightAdj.cs
+++ Assets/Scripts/AddCardFunction/Adjective/LightAdj.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class LightAdj : IAdjective
@@ -7,6 +10,13 @@ public class LightAdj : IAdjective
     private EAdjective adjectiveName = EAdjective.Light;
     private EAdjectiveType adjectiveType = EAdjectiveType.Normal;
     private int count = 0;
+
+    #region LightAdjClassMem
+
+    private bool isLighting;
+    private GameObject lightSource;
+
+    #endregion
     
     public EAdjective GetAdjectiveName()
     {
@@ -31,11 +41,16 @@ public class LightAdj : IAdjective
     public void Execute(InteractiveObject thisObject)
     {
         //Debug.Log("this is Light");
-        GameObject gameObject = new GameObject("Light");
-        gameObject.transform.SetParent(thisObject.transform);
-        gameObject.transform.localPosition = new Vector3(0, 0.5f, 0);
-        gameObject.AddComponent<Light>();
-        gameObject.GetComponent<Light>().intensity = 5;
+        // GameObject gameObject = new GameObject("Light");
+        // gameObject.transform.SetParent(thisObject.transform);
+        // gameObject.transform.localPosition = new Vector3(0, 0.5f, 0);
+        // gameObject.AddComponent<Light>();
+        // gameObject.GetComponent<Light>().intensity = 5;
+    
+        if (!isLighting)
+        {
+            LetThereBeLight(thisObject.gameObject);
+        }
     }
 
     public void Execute(InteractiveObject thisObject, GameObject player)
@@ -46,5 +61,45 @@ public class LightAdj : IAdjective
     public void Execute(InteractiveObject thisObject, InteractiveObject otherObject)
     {
         //Debug.Log("Light : this Object -> other Object");
+    }
+
+    public void Abandon(InteractiveObject thisObject)
+    {
+        if (isLighting)
+        {
+            LeteThereNoLight(thisObject.gameObject);
+        }
+    }
+
+
+    private void LetThereBeLight(GameObject targetObj)
+    {
+        isLighting = true;
+        var lightSource =FindEffect("LightEffect");
+        GameObject.Instantiate(lightSource, targetObj.transform);
+    }
+    
+    void LeteThereNoLight(GameObject targetObj)
+    {
+        if (isLighting)
+        {
+            isLighting = false;
+            // lightSource.GetComponent<Light>().enabled = false;
+            Debug.Log(targetObj.GetComponentInChildren<Light>().gameObject.name,targetObj.GetComponentInChildren<Light>().gameObject.transform);
+            GameObject.Destroy(targetObj.GetComponentInChildren<Light>().gameObject);
+            // GameObject.Destroy(lightSource);
+
+        }
+    }
+    GameObject FindEffect(String prefabName)
+    {
+        string path = "Assets/Prefabs/Effect/" + prefabName + ".prefab";
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        if (prefab == null)
+        {
+            Debug.LogError("Prefab not found: " + prefabName);
+        }
+
+        return prefab;
     }
 }
