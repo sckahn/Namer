f292f8777d963dae6478dba81f15b2bfd830e037
heokiro <87606800+heokiro@users.noreply.github.com>
2023-02-01 18:16:26 +0900

    [Fix] ReturnBtn
    NC-174
    1. 크레딧 씬에서 일시정지 한 상태로 리턴버튼 눌렀을때 일시정지를 풀어줌

--- Assets/Scripts/UI/MainUIController.cs
+++ Assets/Scripts/UI/MainUIController.cs
@@ -180,6 +180,7 @@ public class MainUIController : MonoBehaviour
     {
         if (state == MainMenuState.Credit)
         {
+            Time.timeScale = 1;
             MainMenuScene();
         }
     }
