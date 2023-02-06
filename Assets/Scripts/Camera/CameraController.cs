da033fd59bd4c76e72db3ee34e27b0f9013ece7f
MoHoDu <imvagueness@gmail.com>
2023-02-06 15:52:43 +0900

    [Add] 팔로우 카메라 추가, 탑뷰 기능 추가
    NC-203 카메라 탑뷰 구현
    NC-204 팔로우 카메라 구현
    
    1) 시네머신 패키지 추가
    2) 시네머신 virtual camera로 탑뷰, 쿼터뷰 캠 추가 및 프리팹화
    3) CameraController.cs의 키 입력을 통해 카메라 시점 변화
    
    [참고]
    https://www.notion.so/flearnerhq/0fb30a6acf8a4d51bc7868f319c96cd1

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    bool isOn = false;
+    int normalCamPirority;
+    [SerializeField] KeyCode cameraKey;
+    [SerializeField] CinemachineVirtualCamera playerTopViewCam;
+    [SerializeField] CinemachineVirtualCamera playerNormalViewCam;
+
+    void Awake()
+    {
+        normalCamPirority = playerNormalViewCam.Priority;
+        cameraKey = KeyCode.Q;
+    }
+
+    void Update()
+    {
+        // 누르고 있는 동안 탑뷰로 하기 
+        //if (isOn != Input.GetKey(cameraKey))
+        //{
+        //    isOn = Input.GetKey(cameraKey);
+        //    playerTopViewCam.Priority = normalCamPirority + (isOn ? 1 : -1);
+        //}
+
+        // 토글로 탑뷰 하기
+        if (Input.GetKeyDown(cameraKey))
+        {
+            isOn = !isOn;
+            playerTopViewCam.Priority = normalCamPirority + (isOn ? 1 : -1);
+        }
+    }
+}
