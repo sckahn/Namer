7b094b69f7eeedf267227021241c018e6ae863a0
MoHoDu <imvagueness@gmail.com>
2023-02-06 17:25:30 +0900

    [Add] 특정 오브젝트 포커스 카메라 구현
    NC-254
    
    1) 특정 오브젝트를 포커스 하거나 포커스를 끄는 virtual camera와 FocusOn(Transform), FocusOff() 를 구현
    2) 카메라의 우선 순위를 enum으로 변경

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,16 +5,52 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    enum PriorityOrder
+    {
+        BehindAtAll = 8,
+        BehingByNormal,
+        normal,
+        FrontByNormal,
+        FrontAtAll
+    }
+
     bool isOn = false;
     int normalCamPirority;
+
     [SerializeField] KeyCode cameraKey;
     [SerializeField] CinemachineVirtualCamera playerTopViewCam;
     [SerializeField] CinemachineVirtualCamera playerNormalViewCam;
+    [SerializeField] CinemachineVirtualCamera targetCam;
+
+    // test
+    [SerializeField] Transform targetObj;
 
     void Awake()
     {
+        Init();
+    }
+
+    public void Init()
+    {
         if (cameraKey == KeyCode.None) cameraKey = KeyCode.Q;
+        playerNormalViewCam.Priority = (int)PriorityOrder.normal;
+        playerTopViewCam.Priority = (int)PriorityOrder.BehingByNormal;
+        targetCam.Priority = (int)PriorityOrder.BehindAtAll;
         normalCamPirority = playerNormalViewCam.Priority;
+    }
+
+    public void FocusOn(Transform target)
+    {
+        targetCam.LookAt = target;
+        targetCam.Follow = target;
+        targetCam.Priority = (int)PriorityOrder.FrontAtAll;
+    }
+
+    public void FocusOff()
+    {
+        targetCam.Priority = (int)PriorityOrder.BehindAtAll;
+        targetCam.LookAt = null;
+        targetCam.Follow = null;
     }
 
     void Update()
@@ -23,14 +59,29 @@ public class CameraController : MonoBehaviour
         //if (isOn != Input.GetKey(cameraKey))
         //{
         //    isOn = Input.GetKey(cameraKey);
-        //    playerTopViewCam.Priority = normalCamPirority + (isOn ? 1 : -1);
+        //    playerTopViewCam.Priority = (isOn ? (int)PriorityOrder.FrontByNormal : (int)PriorityOrder.BehingByNormal);
         //}
 
         // 토글로 탑뷰 하기
         if (Input.GetKeyDown(cameraKey))
         {
             isOn = !isOn;
-            playerTopViewCam.Priority = normalCamPirority + (isOn ? 1 : -1);
+            playerTopViewCam.Priority = (isOn ? (int)PriorityOrder.FrontByNormal : (int)PriorityOrder.BehingByNormal);
         }
     }
+
+#region Test
+    [ContextMenu("FocusOn")]
+    public void TestFocusOn()
+    {
+        if (targetObj == null) return;
+        FocusOn(targetObj);
+    }
+
+    [ContextMenu("FocusOff")]
+    public void TestFocusOff()
+    {
+        FocusOff();
+    }
+    #endregion
 }
