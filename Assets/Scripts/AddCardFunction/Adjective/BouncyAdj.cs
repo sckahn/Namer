38f15a319e8d13429380463ef28c3fa162761728
J1nTech <106944708+J1nTech@users.noreply.github.com>
2023-02-08 16:21:11 +0900

    [Fix] 통통 이팩트 구현 *미완성*
    NC-235
    통통 이팩트 현재까지 구현한 내용 커밋입니다

--- Assets/Scripts/AddCardFunction/Adjective/BouncyAdj.cs
+++ Assets/Scripts/AddCardFunction/Adjective/BouncyAdj.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +13,20 @@ public class BouncyAdj : IAdjective
     private GameObject myObj;
     private Rigidbody rigid;
     [SerializeField] private float jumpPower = 4.528313f;
+
+    private float currentTime;
+    private float movingSpeed = 1f;
+    private Vector3 targetPos;
+    // private Vector3 startPos;
+
+    private bool isBouncy;
+    float bounceHeight = 2f;
+    float bounceSpeed = 3f;
+    float bounciness = 4f;
+
+    private float time;
+    
+    
     #endregion
 
     public EAdjective GetAdjectiveName()
@@ -35,20 +51,14 @@ public class BouncyAdj : IAdjective
     
     public void Execute(InteractiveObject thisObject)
     {
-        if (myObj == null) myObj = thisObject.gameObject;
-        if (rigid == null) rigid = myObj.GetComponent<Rigidbody>();
-        Collider collider = thisObject.GetComponent<BoxCollider>();
 
-        // CheckSurrounding checkingMachine = GameManager.GetInstance.GetCheckSurrounding;
-        var adjacentsDictionary = DetectManager.GetInstance.GetAdjacentsDictionary(thisObject.gameObject, thisObject.transform.lossyScale);
-        
-        // List<Transform> hits = checkingMachine.GetTransformsAtDirOrNull(thisObject.gameObject, Dir.down);
-        List<GameObject> hits = adjacentsDictionary[Dir.down];
-        if (hits != null)
-        {
-            if (rigid.velocity.y < 0 && (thisObject.transform.position.y - hits[0].transform.position.y) <= 1.2f)
-                Bounce();
-        }
+        Vector3 startPos = thisObject.gameObject.transform.position;
+        var rb = thisObject.gameObject.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        CheckBouncible(thisObject.gameObject);
+        thisObject.StartCoroutine(BounceCoroutine(thisObject.gameObject,startPos));
+        // InteractionSequencer.GetInstance.CoroutineQueue.Enqueue(BounceCoroutine(thisObject.gameObject));
     }
 
     public void Execute(InteractiveObject thisObject, GameObject player)
@@ -72,4 +82,53 @@ public class BouncyAdj : IAdjective
         // 플레이어가 위에 있으면, 힘을 더 주도록 해야함 
         rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
     }
+
+    Vector3 SetTargetPos(GameObject targetObj)
+    {
+        Vector3 startPos = targetObj.transform.position;
+        return startPos;
+    }
+
+    void CheckBouncible(GameObject targetObj)
+    {
+        var underObject = DetectManager.GetInstance.GetAdjacentObjectWithDir(targetObj, Dir.down, targetObj.transform.lossyScale);
+        var upperObject =
+            DetectManager.GetInstance.GetAdjacentObjectWithDir(targetObj, Dir.up, targetObj.transform.lossyScale);
+        if (underObject != null && upperObject == null)
+        {
+            isBouncy = true;
+        }
+    }
+    /*
+     * -------TestCase---------
+     * 1. 캐릭터가 아래로 들어갔을때 -> 캐릭터를 박아버리네 
+     * 2. 뛰면서 2단계위에 뭐가 있을때
+     * 3. 뛰면서 위에 캐릭터 있고 그 위에 물체가 있을때
+     * 4. 
+     */
+  
+    private IEnumerator BounceCoroutine(GameObject obj,Vector3 startPos)
+    {
+        float time = 0;
+        Debug.Log(obj,obj.transform);
+        while (isBouncy)
+        {
+            time += Time.deltaTime * bounceSpeed;
+            float y = Mathf.Clamp(Mathf.Sin(time) * bounceHeight,0,1.1f);
+            // float y = Mathf.Sin(time) * bounceHeight;
+            Debug.DrawLine(obj.transform.position,new Vector3(obj.transform.position.x,obj.transform.position.y -1f,obj.transform.position.z),Color.red);
+            obj.transform.position = Vector3.Lerp(obj.transform.position, startPos + new Vector3(0, y, 0), Time.deltaTime * bounciness);
+            
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    
+    /*
+     * 1. 크기만큼 y축으로 올라가따가 내려가따가
+     * 1. check if it can go up
+     * 2. make it go up and down continuously
+     * 
+     * 2. 캐릭터가  위에 올라갈수 있음 
+     */
+    
 }
