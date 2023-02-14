3114689c4ac39e81987b143e8503dbe8eeb2e58d
newHj9 <108648161+newHj9@users.noreply.github.com>
2023-02-14 16:17:48 +0900

    [Add] 게임 데이터의 카드 정보로 카드 프리팹 UI 텍스트에 표시
    #NC-317
    1. 게임 데이터의 카드 정보로 카드 프리팹 UI 텍스트에 표시

--- Assets/Scripts/Card/CardController.cs
+++ Assets/Scripts/Card/CardController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class CardController : MonoBehaviour
 {
@@ -15,6 +15,8 @@ public class CardController : MonoBehaviour
     [SerializeField] BoxCollider bc;
     [SerializeField] GameObject highlight;
     [SerializeField] GameObject Encyclopedia;
+    [SerializeField] private Text UIText;
+    [SerializeField] private Text NameAdjUIText;
     CardRotate cr;
 
     public EAdjective GetAdjectiveTypeOfCard()
@@ -26,6 +28,53 @@ public class CardController : MonoBehaviour
     {
         cr = this.gameObject.GetComponent<CardRotate>();
         cardHolder = FindObjectOfType<CardManager>().gameObject;
+
+        SetUIText();
+    }
+
+    private void SetUIText()
+    {
+        if (UIText == null || (cardType == ECardType.Name && NameAdjUIText == null))
+        {
+            Debug.LogError("인스펙터창에서 UI 텍스트를 확인 후 넣어주세요!");
+        }
+        
+        GameDataManager gameDataManager = GameDataManager.GetInstance;
+        if (cardType == ECardType.Name)
+        {
+            UIText.text = gameDataManager.Names[nameType].uiText;
+            NameAdjUIText.text = "";
+            
+            if (gameDataManager.Names[nameType].adjectives != null)
+            {
+                EAdjective[] adjectives = gameDataManager.Names[nameType].adjectives;
+                string delimiter = ", ";
+                for (int i = 0; i < adjectives.Length; i++)
+                {
+                    NameAdjUIText.text += gameDataManager.Adjectives[adjectives[i]].uiText + delimiter;
+                }
+                
+                NameAdjUIText.text = NameAdjUIText.text.TrimEnd(',', ' ');
+            }
+            else
+            {
+                NameAdjUIText.text = "순수";
+            }
+        }
+        else if (cardType == ECardType.Adjective)
+        {
+            UIText.text = gameDataManager.Adjectives[adjectiveType].uiText;
+        }
+
+        Text contentText = Encyclopedia.GetComponentInChildren<Text>();
+        if (cardType == ECardType.Name)
+        {
+            contentText.text = gameDataManager.Names[nameType].contentText;
+        }
+        else if (cardType == ECardType.Adjective)
+        {
+            contentText.text = gameDataManager.Adjectives[adjectiveType].contentText;
+        }
     }
 
     public void MoveTransform(PRS prs, bool useDotween, float dotweenTime = 0)
