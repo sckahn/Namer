8e32d48253af14ccc7b02a1ca7d1759a2dd5b028
J1nTech <106944708+J1nTech@users.noreply.github.com>
2023-02-16 12:42:05 +0900

    [fix] 리셋 버그 수정
    NC-333
    1)리셋 후 리셋 처음부터 시작 되지않는 현상 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -371,6 +371,7 @@ public class GameManager : Singleton<GameManager>
         GetNewCardDeck();
         cameraController.Init();
         scenarioController.Init();
+        resetLoadValue = 0f;
     }
 
     
